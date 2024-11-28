8a4f1939e0d0aea4dbe5d6cf40eaeb7225499814
JoepH123 <joep.hillenaar2@gmail.com>
2024-11-28 14:21:45 +0100

    speed up by not loading from xes everytime
    also added requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from src.load_and_prepare_data import load_data_to_pd
+from src.load_and_prepare_data import load_data_xes_to_pd
+import pandas as pd
+import os
 
-def main(path):
-    load_data_to_pd(path)
+def main(data_location, file_name):
+    file_name_no_ext = file_name.split(".")[0]
+    file_path_gzip = f"{data_location}\{file_name_no_ext}.gzip"
+    if not os.path.isfile(file_path_gzip):
+        file_path_xes = f"{data_location}\{file_name}"
+        df = load_data_xes_to_pd(file_path_xes)
+    else:
+        df = pd.read_parquet(file_path_gzip)
+    print(df)
+    return df
+
 
 if __name__ == "__main__":
-    data_location = ".."
+    data_location = "data"
     file_name = "BPI_Challenge_2017.xes.gz"
-    path = f"{data_location}\{file_name}"
-    main(path)
+    main(data_location, file_name)
--- src/load_and_prepare_data.py
+++ src/load_and_prepare_data.py
@@ -3,7 +3,8 @@
 import pm4py
 
 
-def load_data_to_pd(path):
+def load_data_xes_to_pd(path):
     log = pm4py.read_xes(path)
-    pd = pm4py.convert_to_dataframe(log)
-    return pd
+    df = pm4py.convert_to_dataframe(log)
+    df.to_parquet("data/BPI_Challenge_2017.gzip")  # Save to gzip so that we can more quickly open to pandas dataframe
+    return df
